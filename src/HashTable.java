85ce08d251f423395303914190fea59ce6a55462
fsjoyti <fsjoyti@iastate.edu>
2017-02-14 13:09:16 -0600

    Started working on hashtable

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,4 +1,34 @@
 
 public class HashTable {
+	
+	
+	
 
+	public HashTable(int size){
+		size = setinitialp(size);
+		
+	
+		
+	}
+	
+	private int setinitialp(int range) {
+		while (!isPrime(range)){
+			  range ++;
+		  }
+		return range;
+	}
+	
+	private boolean isPrime(int num){
+		if (num%2==0)
+			return false;
+		for (int i = 3; i*i <= num; i++){
+			if (num%i == 0){
+				return false;
+			}
+			
+		}
+		return true;
+		
+	}
+	
 }
