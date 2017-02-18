e476a55c5f0347b63ce79421a1af24906d08ab2b
fsjoyti <fsjoyti@iastate.edu>
2017-02-17 20:19:01 -0600

    Modified

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,17 +1,50 @@
 
+
+import java.util.*;
 public class HashTable {
 	
+	private LinkedList<Tuple>[] hashtable;
+	private HashFunction h;
+	private int sizeofHashTable;
+	private int maxLoad;
+	private int avgLoad;
 	
-	
-
+	@SuppressWarnings("unchecked")
 	public HashTable(int size){
-		size = setinitialp(size);
+		sizeofHashTable = setsize(size);
+		h = new HashFunction(sizeofHashTable );
+		hashtable = new LinkedList[sizeofHashTable ];
+		for (int i= 0; i < sizeofHashTable ; i++){
+			hashtable[i] = new LinkedList<Tuple>();
+		}
+		
 		
 	
 		
 	}
 	
-	private int setinitialp(int range) {
+	
+	public int maxLoad(){
+		
+		return maxLoad;
+	}
+	
+	
+	public int averageLoad(){
+		
+		return avgLoad;
+	}
+	
+	
+	public void add(Tuple t){
+		int size_new = sizeofHashTable;
+		sizeofHashTable = setsize(size_new);
+		
+		
+	}
+	
+	
+	private int setsize(int range) {
 		while (!isPrime(range)){
 			  range ++;
 		  }
